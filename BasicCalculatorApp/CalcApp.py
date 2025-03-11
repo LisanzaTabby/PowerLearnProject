1c42cced128a569786fcb63ee2123e6fb118cf51
lisanzatabby <lisanzatabitha@gmail.com>
2025-03-11 14:42:40 +0300

    Basic calc

--- BasicCalculatorApp/CalcApp.py
+++ BasicCalculatorApp/CalcApp.py
@@ -6,11 +6,13 @@ operation = input("Enter operation(+,/,*,or-):")
 if operation == "+":
     result = number1 + number2
 elif operation == "/":
+
     if number2 == 0:
         print("Error: Divisor can't be Zero(0)")
         result = None
     else:
         result = number1 / number2
+        
 elif operation == "*":
     result = number1 * number2
 elif operation == "-":
