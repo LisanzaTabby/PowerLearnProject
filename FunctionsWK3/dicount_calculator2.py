5a05aaf769335fc4ef27630a768ef76be263644f
lisanzatabby <lisanzatabitha@gmail.com>
2025-03-21 12:44:50 +0300

    Complete WK3 Assignment

---
+++ FunctionsWK3/dicount_calculator2.py
@@ -0,0 +1,11 @@
+# Discount_calculator function that prompts a user for Original price and discount_percent
+def calculate_discount():
+    price = int(input("Enter Original price of an item: "))
+    discount_percent = float(input("Enter discount_percentage for the item(decimal format i.e. 0.2)& Zero if no discount: "))
+    if discount_percent == 0:
+        return price
+    else:
+        final_price = price + (price*discount_percent)
+        return final_price
+    
+print(calculate_discount())
---
+++ FunctionsWK3/discount_calculator1.py
@@ -0,0 +1,10 @@
+# Calculate_dicount function that takes an original price and calculates the discount on the price if its <=20% else should return the original price
+def calculate_discount(price, discount_percent):
+    if discount_percent >= 0.2:
+        final_price= price + (price*discount_percent)
+        return final_price
+    else:
+        return price
+    
+print(calculate_discount(30000, 0.2))# 20% discount
+print(calculate_discount(40000, 0.1))# 10% discount is less than 20% returns the original price
