9f0d3a07e81b9a35ccec814c3f398b7a3f6a8582
lisanzatabby <lisanzatabitha@gmail.com>
2025-04-15 19:35:42 +0300

    Python OOP concepts

---
+++ PythonOOP/oop2.py
@@ -0,0 +1,39 @@
+# Question 2
+# Base class
+class Vehicle:
+    def move(self):
+        print("The vehicle is moving.")
+
+# Subclass 1
+class Car(Vehicle):
+    def move(self):
+        print("🚗 The car is driving on the road.")
+
+# Subclass 2
+class Plane(Vehicle):
+    def move(self):
+        print("✈️ The plane is flying in the sky.")
+
+# Subclass 3
+class Bicycle(Vehicle):
+    def move(self):
+        print("🚴 The bicycle is pedaling on the path.")
+
+# Subclass 4
+class Boat(Vehicle):
+    def move(self):
+        print("🚢 The boat is sailing on water.")
+
+# Polymorphism in action
+def show_movement(vehicle):
+    vehicle.move()
+
+# Create objects
+car = Car()
+plane = Plane()
+bicycle = Bicycle()
+boat = Boat()
+
+# Use the same function with different objects
+for v in [car, plane, bicycle, boat]:
+    show_movement(v)
